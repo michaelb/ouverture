dfa055fbde4d7e6b124a5ca2a01df5dff57e1889
Michael Bleuez <michael.bleuez2@gmail.com>
2021-12-26 00:52:46 +0100

    ctrl-c-able ctrl-c and other goodies

--- ouverture-core/src/main.rs
+++ ouverture-core/src/main.rs
@@ -1,25 +1,23 @@
 mod config;
+mod logger;
 mod music;
 mod opt;
 mod server;
-use crate::server::{Command::Stop, Server};
-use chrono;
-use color_eyre::eyre::eyre;
-use color_eyre::{eyre::Report, eyre::WrapErr, Result, Section};
-use fern::colors::{Color, ColoredLevelConfig};
-use log::LevelFilter::*;
-use log::{debug, error, info, trace, warn};
-use opt::Opt;
-use structopt::StructOpt;
 
+use crate::server::{Command::Stop, Server};
+use color_eyre::eyre::eyre;
+use color_eyre::Result;
+use futures::stream::StreamExt;
+use log::LevelFilter::*;
+use log::{info, warn};
+use logger::{setup_logger, LogDestination::*};
+use opt::Opt;
 use ouverture_core::config::Config;
 use ouverture_core::start;
-
-mod logger;
-use logger::{setup_logger, LogDestination::*};
-use futures::stream::StreamExt;
 use signal_hook::consts::signal::*;
 use signal_hook_tokio::Signals;
+use std::sync::{Arc, Mutex};
+use structopt::StructOpt;
 
 #[tokio::main]
 async fn main() -> Result<()> {
@@ -51,37 +49,45 @@ async fn main() -> Result<()> {
     info!("Config : {:?}", config);
 
     // Set up signal handlers
+    let first_signal = Arc::new(Mutex::new(true));
     let address = config.server_address.clone() + ":" + &config.server_port.clone();
-    let signals = Signals::new(&[
-        SIGTERM,
-        SIGINT,
-        SIGQUIT,
-    ])?;
+    let signals = Signals::new(&[SIGTERM, SIGINT, SIGQUIT])?;
     let handle = signals.handle();
-    let signals_task = tokio::spawn(handle_signals(signals, address));
+    let signals_task = tokio::spawn(handle_signals(signals, address, first_signal.clone()));
 
-    // Start ouverture server
+    // Start ouverture server (unique async entry point)
     let res = start(config).await;
+    let return_value = match res {
+        Err(e) => {
+            warn!("Server exited with an error: {:?}", e);
+            Err(eyre!(format!("{:?}", e)))
+        }
+        _ => Ok(()),
+    };
 
     handle.close();
     signals_task.await?;
 
-
-    match res {
-        Err(e) => Err(eyre!(format!("{:?}", e))),
-        Ok(_) => Ok(()),
+    // If a stop signal was received, exit with non-zero status
+    if !*first_signal.lock().unwrap() {
+        std::process::exit(1);
     }
+    return_value
 }
 
-async fn handle_signals(signals: Signals, address: String) {
+async fn handle_signals(signals: Signals, address: String, first_signal: Arc<Mutex<bool>>) {
     let mut signals = signals.fuse();
     while let Some(signal) = signals.next().await {
         match signal {
             SIGTERM | SIGINT | SIGQUIT => {
                 // Shutdown the system;
-                Server::send(&Stop, &address).await.unwrap();
-
-            },
+                if *first_signal.lock().unwrap() {
+                    *first_signal.lock().unwrap() = false;
+                    Server::send(&Stop, &address).await.unwrap();
+                } else {
+                    std::process::exit(signal);
+                }
+            }
             _ => unreachable!(),
         }
     }
