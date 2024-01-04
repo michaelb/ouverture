170934deb3fa766e778c33ba7dfbffb27190616a
Michael B <michael.bleuez2@gmail.com>
2024-01-04 16:15:15 +0100

    remove obsolete code

--- ouverture-core/src/server.rs
+++ ouverture-core/src/server.rs
@@ -9,16 +9,15 @@ use std::sync::{Arc, Mutex};
 
 use strum_macros::{Display, EnumIter, EnumString};
 use tokio::io::{AsyncReadExt, AsyncWriteExt};
-use tokio::net::{TcpStream};
+use tokio::net::TcpStream;
 
 use std::sync::atomic::Ordering;
 
 use crate::config::Config;
 use crate::error::ServerError;
-use crate::{library::*, STOP_FLAG};
 use crate::music::song::Song;
+use crate::{library::*, STOP_FLAG};
 use color_eyre::Result;
-
 
 use crate::audio::AudioState;
 
@@ -72,178 +71,6 @@ impl Server {
 
         Ok(())
     }
-
-    async fn handle_command(
-        command: Command,
-        config: Config,
-        audio_state: Arc<Mutex<AudioState>>,
-        mut socket: &mut TcpStream,
-    ) {
-        match command {
-            Command::Play(opt_song) => audio_state.lock().unwrap().play(opt_song),
-            Command::Toggle => audio_state.lock().unwrap().toggle(),
-
-            Command::Pause => audio_state.lock().unwrap().pause(),
-            Command::Enqueue(song) => audio_state.lock().unwrap().enqueue(song),
-
-            Command::Next => audio_state.lock().unwrap().next(),
-            Command::Previous => audio_state.lock().unwrap().previous(),
-
-            Command::Scan => scan(&config).await,
-            Command::GetList(i) => {
-                let list = list(&config, i).await;
-                match Self::reply(Reply::List(list), &mut socket).await {
-                    Ok(_) => trace!("Replied 'list' successfully"),
-                    Err(e) => {
-                        warn!("Failed to send 'list' reply to client: {:?}", e)
-                    }
-                }
-            }
-
-            Command::GetCurrentSong => {
-                let current_song = audio_state.lock().unwrap().current_song.clone();
-                let current_seek = AudioState::get_seek(audio_state).await;
-                match Self::reply(
-                    Reply::CurrentSong(current_song.clone(), current_seek),
-                    &mut socket,
-                )
-                .await
-                {
-                    Ok(_) => trace!(
-                        "Replied 'current song is {current_song:?}' (seek = {}%) successfully",
-                        current_seek * 100.0
-                    ),
-                    Err(e) => {
-                        warn!("Failed to send 'list' reply to client: {:?}", e)
-                    }
-                }
-            }
-
-            Command::Seek(seek) => audio_state.lock().unwrap().set_seek(seek),
-
-            Command::Ping => (),
-            Command::Restart => (),
-            Command::Stop => {
-                // state.lock().unwrap().stop = true;
-            }
-        };
-    }
-
-    // clients can use this helper to send commands to a server
-    // and wait till the server replies 'done'
-    pub async fn send_wait(
-        message: &Command,
-        address: &str,
-    ) -> Result<Reply, Box<dyn Error + Send + Sync>> {
-        let encoded: Vec<u8> = message.prepare_query()?;
-        let mut stream = TcpStream::connect(address).await?;
-        stream.write_all(&encoded).await?;
-
-        // Wait for 'done', displaying all other replies
-        let mut buf = [0u8; 8];
-        loop {
-            match stream.read(&mut buf).await {
-                // socket closed
-                Ok(n) if n == 0 => break,
-                Ok(n) => n,
-                Err(e) => {
-                    error!("failed to read from socket; err = {:?}", e);
-                    break;
-                }
-            };
-
-            let size = match Reply::decode_size(buf) {
-                Ok(i) => i,
-                Err(e) => {
-                    trace!("error: {:?}, stopping", e);
-                    break;
-                }
-            };
-
-            let mut payload = vec![0; size as usize];
-            let res = stream.read_exact(&mut payload[..]).await;
-            trace!("res from socket = {:?}", res);
-
-            let decoded_reply = bincode::deserialize::<Reply>(&payload);
-            if let Ok(ok_reply) = decoded_reply {
-                //only return the meaningful value
-                match ok_reply {
-                    Reply::Received(_) => continue,
-                    _ => return Ok(ok_reply),
-                }
-            }
-        }
-        Err("Communication with the server failed".into())
-    }
-
-    // sync-callable version that creates its own tokio runtime
-    // to wait on tcp response stuff
-    pub fn send_wait_sync(
-        message: &Command,
-        address: &str,
-    ) -> Result<Reply, Box<dyn Error + Send + Sync>> {
-        let rt = Runtime::new().unwrap();
-        rt.block_on(Server::send_wait(message, address))
-    }
-
-    // send a command to the server and receive an stream to listen to the server's responses
-    pub async fn send<'a>(
-        message: &'a Command,
-        address: &'a str,
-    ) -> impl Stream<Item = Result<Reply, Box<dyn Error + Send + Sync>>> + 'a {
-        try_stream! {
-              let encoded: Vec<u8> = message.prepare_query()?;
-              let mut stream = TcpStream::connect(address).await?;
-              stream.write_all(&encoded).await?;
-               // Wait for 'done', yielding all other replies
-              let mut buf = [0u8; 8];
-              loop {
-                  match stream.read(&mut buf).await {
-                      // socket closed
-                      Ok(n) if n == 0 => break,
-                      Ok(n) => n,
-                      Err(e) => {
-                          error!("failed to read from socket; err = {:?}", e);
-                          break;
-                      }
-                  };
-
-        let size = match Reply::decode_size(buf){
-                  Ok(i) => i,
-                  Err(e) => {trace!("error: {:?}, stopping",e); break;},
-              };
-
-
-                  let mut payload = vec![0; size as usize];
-                  let res = stream.read_exact(&mut payload[..]).await;
-                  trace!("res from socket = {:?}", res);
-
-                  let decoded_reply = bincode::deserialize::<Reply>(&payload)?;
-               match decoded_reply {
-                   Reply::Done => { yield Reply::Done; break },
-                   r => yield r,
-               }
-
-
-              }
-
-          }
-    }
-
-    async fn reply(
-        reply: Reply,
-        stream: &mut TcpStream,
-    ) -> Result<(), Box<dyn Error + Send + Sync>> {
-        let encoded_reply: Vec<u8> = reply.prepare_query()?;
-        stream.write_all(&encoded_reply).await?;
-        Ok(())
-    }
-    // async fn reply(reply: Reply, address: &str) -> Result<(), Box<dyn Error + Send + Sync>> {
-    //        let encoded_reply: Vec<u8> = reply.prepare_query()?;
-    //        let mut stream = TcpStream::connect(address).await?;
-    //        stream.write_all(&encoded_reply).await?;
-    //        Ok(())
-    //    }
 }
 
 #[non_exhaustive]
